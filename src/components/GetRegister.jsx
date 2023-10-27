1320c72ba1d3278f0233b266608ce7a849ddacef
Christan <rajakoonechristan@gmail.com>
2023-10-27 19:58:19 +0530

    check

--- src/components/GetRegister.jsx
+++ src/components/GetRegister.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import { arrowup } from "../assets";
 
 const GetRegister = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-white p-[2px] cursor-pointer left-7`}>
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-white p-[2px] cursor-pointer left-7 sm:mt-100 mt-30`}>
     <div className={`${styles.flexCenter} flex-col bg-black w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
